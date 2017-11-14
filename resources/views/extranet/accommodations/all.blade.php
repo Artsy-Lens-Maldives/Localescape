94e76e525ae5d43e8ec4670a1168fdc83369fdba
athik13 <athik.13@gmail.com>
2017-11-14 10:50:36 +0500

    done my accommodation

--- resources/views/extranet/accommodations/all.blade.php
+++ resources/views/extranet/accommodations/all.blade.php
@@ -12,57 +12,30 @@
                 <div class="title">
                     <h1><a href="#">My Accommodations</a></h1>
                 </div>
-                <div class="my-items table-responsive">
-                    <table class="table">
-                        <thead>
+                <div class="reservations table-responsive">
+                    <table class="table table-fixed-header">
+                        <thead class="header">
                         <tr>
-                            <th>Accommodation</th>
-                            <th>Featured</th>
-                            <th>Views</th>
-                            <th>Reviews</th>
-                            <th>Rating</th>
-                            <th>Last Booking</th>
+                            <th>Accommodation Name</th>
+                            <th>Accommodation Type</th>
+                            <th>Location</th>
+                            <th>Created at</th>
+                            <th>Action</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <tr class="my-item">
+                        <tr class="reservation">
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
                             <td>
-                                <div class="image-wrapper">
-                                    <div class="mark-circle top" data-toggle="tooltip" data-placement="right" title="Top accommodation"><i class="fa fa-thumbs-up"></i></div>
-                                    <a href="{{ url('extranet/accommodations/edit') }}" class="image">
-                                        <div class="bg-transfer">
-                                            <img src="/assets/img/items/01.jpg">
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="info">
-                                    <a href="/hotels/detail"><h2>Accommodation 1</h2></a>
-                                    <figure class="location">Location</figure>
-                                    <figure class="label label-info">Hotel</figure>
-                                    <div class="meta">
-                                        <span><i class="fa fa-bed"></i>365</span>
-                                        <span class="price-info">From <span class="price">$32</span><span class="appendix">/night</span></span>
-                                    </div>
-                                    <!--end meta-->
-                                </div>
-                                <!--end info-->
-                            </td>
-                            <td><div class="featured yes"><i class="fa fa-check"></i><aside></aside></div></td>
-                            <td class="views">426</td>
-                            <td class="reviews">45</td>
-                            <td class="rating">9.3</td>
-                            <td class="last-reservation">Today 15:32
-                                <span class="last-edit">Last edited: Today 12:35</span>
-                                <div class="edit-options">
-                                    <a href="{{ url('extranet/accommodations/edit') }}" class="link icon"><i class="fa fa-edit"></i>Edit</a>
-                                    <a href="{{ url('extranet/accommodations/bookings') }}" class="link icon"><i class="fa fa-check-square-o"></i>Bookings</a>
-                                    <a href="{{ url('extranet/accommodations/reviews') }}" class="link icon"><i class="fa fa-comment"></i>Reviews</a>
-                                    <a href="#" class="link icon delete"><i class="fa fa-trash"></i>Delete</a>
-                                </div>
-                                <!--end edit-options-->
+                                <a class="btn btn-danger" href="" onclick="return confirm('Are you sure you would like to delete this accomodation. This process cannot be reversed.')">Delete</a>
+                                <a class="btn btn-warning" href="">Edit</a>
+                                <a class="btn btn-info" href="">Rooms</a>
                             </td>
                         </tr>
-                        <!--end my-item-->
+                        <!--end reservation-->
                         </tbody>
                     </table>
                 </div>
@@ -71,4 +44,20 @@
             <!--end main-content-->
         </div>
         <!--end container-->    
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
+@endsection
+
+@section('js')
+    <script src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
+
+    <script>
+    $(document).ready(function() {
+        $('#example').DataTable();
+    } );
+    </script>
+
 @endsection
