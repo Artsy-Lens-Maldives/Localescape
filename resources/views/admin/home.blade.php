f7cadd122e910ce14a4a364d7ce96617499a675e
athik13 <athik.13@gmail.com>
2017-11-30 14:49:33 +0500

    ready for home page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,3 +5,14 @@
 
 
 @endsection
+
+@section('js')
+    <script src="{{ url('js/admin-home.js') }}"></script>
+    <script src="{{ url('js/daterangepicker.js') }}"></script>
+    <script src="{{ url('js/date.js') }}"></script>
+    <script src="{{ url('js/moment-with-locales.min.js') }}"></script>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="{{ url('css/daterangepicker.css') }}">
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -27,6 +27,12 @@ mix.sass('resources/assets/sass/style.scss', 'public/css')
         'node_modules/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css',
         'node_modules/gentelella/vendors/font-awesome/css/font-awesome.min.css',
         'node_modules/gentelella/vendors/nprogress/nprogress.css',
+        
+        'node_modules/gentelella/vendors/iCheck/skins/flat/green.css',
+        'node_modules/gentelella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css',
+        'node_modules/gentelella/vendors/jqvmap/dist/jqvmap.min.css',
+        'node_modules/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.css',
+
         'node_modules/gentelella/vendors/dropzone/dist/min/dropzone.min.css',
         'node_modules/gentelella/build/css/custom.min.css',
     ], 'public/css/admin.css')
@@ -37,3 +43,21 @@ mix.sass('resources/assets/sass/style.scss', 'public/css')
         'node_modules/gentelella/vendors/dropzone/dist/min/dropzone.min.js',
         'node_modules/gentelella/build/js/custom.min.js',
     ], 'public/js/admin.js')
+    .js([
+        'node_modules/gentelella/vendors/Chart.js/dist/Chart.min.js',
+        'node_modules/gentelella/vendors/gauge.js/dist/gauge.min.js',
+        'node_modules/gentelella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js',
+        'node_modules/gentelella/vendors/iCheck/icheck.min.js',
+        'node_modules/gentelella/vendors/skycons/skycons.js',
+        'node_modules/gentelella/vendors/Flot/jquery.flot.js',
+        'node_modules/gentelella/vendors/Flot/jquery.flot.pie.js',
+        'node_modules/gentelella/vendors/Flot/jquery.flot.time.js',
+        'node_modules/gentelella/vendors/Flot/jquery.flot.stack.js',
+        'node_modules/gentelella/vendors/Flot/jquery.flot.resize.js',
+        'node_modules/gentelella/vendors/flot.orderbars/js/jquery.flot.orderBars.js',
+        'node_modules/gentelella/vendors/flot-spline/js/jquery.flot.spline.min.js',
+        'node_modules/gentelella/vendors/flot.curvedlines/curvedLines.js',
+        'node_modules/gentelella/vendors/jqvmap/dist/jquery.vmap.js',
+        'node_modules/gentelella/vendors/jqvmap/dist/maps/jquery.vmap.world.js',
+        'node_modules/gentelella/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js',
+    ], 'public/js/admin-home.js')
