c740cc64d2c047eda49e7930571425b2b40b87fb
athik13 <athik.13@gmail.com>
2018-01-21 18:18:36 +0500

    google verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="author" content="Mohamed Athik">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="3BSOoPE407yris60TCMo3pcKmSXIRXeKPSc1Cw_1_CY" />
     <title>Local Escape</title>
     <!-- CSS Files -->
     <link rel="stylesheet" href="/css/all.css" type="text/css">
