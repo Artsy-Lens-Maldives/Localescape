39c865d919077180d48b1bf09a19f834f40e283d
Yaniu09 <imperialaeiou@gmail.com>
2018-01-26 10:33:02 +0600

    s

--- resources/views/inquery/detail.blade.php
+++ resources/views/inquery/detail.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.admin')
 
 @section('title')
-    <span>Inquiry Detail of  {{ $inquiry->user->name }} as  {{ $inquiry->name }} to {{ $inquiry->room->accommodation->title }} - {{ $inquiry->room->room_type }} </span>
+    <span style="font-size:14px;">Inquiry Detail of  {{ $inquiry->user->name }} as  {{ $inquiry->name }} to {{ $inquiry->room->accommodation->title }} - {{ $inquiry->room->room_type }} </span>
 @endsection
 
 @section('content')
     
     <div class="container">
-      <h3><span style="font-weight:bold;">User Name:</span><br>  {{ $inquiry->name }}</h3>
-      <h3><span style="font-weight:bold;">Email:</span><br>  {{ $inquiry->email }}</h3>
-      <h3><span style="font-weight:bold;">Contact Number:</span><br>  {{ $inquiry->contact }}</h3>
-      <h3><span style="font-weight:bold;">Accomodation Inquiried:</span><br>  {{ $inquiry->room->accommodation->title }} - {{ $inquiry->room->room_type }}</h3>
-      <h3><span style="font-weight:bold;">Accomodation link:</span><br>  localescapemaldives.com/accommodation/(('accommodation_type'))/(('accommodation->slug'))</h3>
-      <h3><span style="font-weight:bold;">Inquiry Detail:</span><br> {{ $inquiry->details }}</h3>
+      <h4><span style="font-weight:bold;">Name:</span><br>  {{ $inquiry->name }}</h4>
+      <h4><span style="font-weight:bold;">Email:</span><br>  {{ $inquiry->email }}</h4>
+      <h4><span style="font-weight:bold;">Contact Number:</span><br>  {{ $inquiry->contact }}</h4>
+      <h4><span style="font-weight:bold;">Accomodation Inquiried:</span><br>  {{ $inquiry->room->accommodation->title }} - {{ $inquiry->room->room_type }}</h4>
+      <h4><span style="font-weight:bold;">Accomodation link:</span><br>  localescapemaldives.com/accommodation/(('accommodation_type'))/(('accommodation->slug'))</h4>
+      <h4><span style="font-weight:bold;">Inquiry Detail:</span><br> {{ $inquiry->details }}</h4>
     </div>
 
 @endsection
