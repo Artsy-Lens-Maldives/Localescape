88aa98b8ef186a323f1131d382706f879cb56c44
Yaniu09 <imperialaeiou@gmail.com>
2018-01-27 00:21:17 +0600

    asd

--- resources/views/mails/customer/booking.blade.php
+++ resources/views/mails/customer/booking.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
-# Hey Customer,
+# Hey Customer Name ,
 
-We have recieved a booking request on Local Escape Boutique -Delexue Room on (today's date 21.05.2018).You will see the booking details below 
+We have recieved a booking request on Local Escape Boutique -Delexue Room on  21.05.20xx .You will see the booking details below 
 
 @component('mail::table')
 | Accomodation Details      |  Price  |
@@ -12,10 +12,10 @@ We have recieved a booking request on Local Escape Boutique -Delexue Room on (to
 @endcomponent
 
 @component('mail::panel')
-Chech In = 21.05.2018	
-Checkout = 25.06.2019	
-Email: imperialaeiou@gmail.com	
-Contact: +9607779423
+Chech In = 21.05.20xx	
+Checkout = 25.06.20xx	
+Email: mailaddress@xxx.com	
+Contact: +960 9xxxxxx
 @endcomponent
 Our Agents will send you a booking confirmation as soon as possible.
 
