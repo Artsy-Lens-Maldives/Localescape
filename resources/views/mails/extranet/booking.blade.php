215c7bdf94a0a620108e1dd37dd1b7904a3691aa
Yaniu09 <imperialaeiou@gmail.com>
2018-01-27 00:44:43 +0600

    s

--- resources/views/mails/extranet/booking.blade.php
+++ resources/views/mails/extranet/booking.blade.php
@@ -1,19 +1,29 @@
 @component('mail::message')
-    # Introduction
+# Hey Customer Name ,
 
-    The body of your message.
+We have recieved a booking request on Local Escape Boutique -Delexue Room on  21.05.20xx .You will see the booking details below 
 
-    @component('mail::table')
-    | Laravel       | Table         | Example  |
-    | ------------- |:-------------:| --------:|
-    | Col 2 is      | Centered      | $10      |
-    | Col 3 is      | Right-Aligned | $20      |
-    @endcomponent
+@component('mail::table')
+| Accomodation Details      |  Price  |
+| ------------- |:---------:|
+| Local Escape Boutique - Deluxe Room       |  $1000      |
+| Tax (12%)      |  $120  |
+| Total      |  $1120  |
+@endcomponent
 
-    @component('mail::button', ['url' => ''])
-    View Booking
-    @endcomponent
+@component('mail::panel')
+Chech In = 21.05.20xx	
+Checkout = 25.06.20xx	
+Email: mailaddress@xxx.com	
+Contact: +960 9xxxxxx
+@endcomponent
+Our Agents will send you a booking confirmation as soon as possible.
 
-    Thanks,<br>
-    {{ config('app.name') }}
+@component('mail::button', ['url' => ''])
+View Booking
+@endcomponent
+
+
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
