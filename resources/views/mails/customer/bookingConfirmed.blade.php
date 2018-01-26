4d01a2348dff928ea4caf368ab29b2d9245f25a2
athik13 <athik.13@gmail.com>
2018-01-26 11:29:07 +0500

    booking confirmed mail

---
+++ resources/views/mails/customer/bookingConfirmed.blade.php
@@ -0,0 +1,24 @@
+@component('mail::message')
+    # Introduction
+
+    The body of your message.
+
+    @component('mail::table')
+    | Laravel       | Table         | Example  |
+    | ------------- |:-------------:| --------:|
+    | Col 2 is      | Centered      | $10      |
+    | Col 3 is      | Right-Aligned | $20      |
+    @endcomponent
+
+    @component('mail::panel')
+    This is the panel content.
+    @endcomponent
+
+    @component('mail::button', ['url' => ''])
+    View Booking
+    @endcomponent
+
+
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
