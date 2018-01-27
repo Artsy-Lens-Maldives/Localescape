dd3fb620ebd7e2f67cb33829c00d118f95995112
Yaniu09 <imperialaeiou@gmail.com>
2018-01-27 16:30:34 +0600

    a

--- resources/views/mails/extranet/signup.blade.php
+++ resources/views/mails/extranet/signup.blade.php
@@ -2,10 +2,11 @@
 # Welcome to {{ config('app.name') }}
 
 Hi {{ $user->name }}
-Thanks so much for joining extranet.
 
-@component('mail::button', ['url' => ''])
-Button Text
+Thanks so much for joining our extranet.If you have any questions do not hesitate to Mail us at info@localescapemaldives.com
+
+@component('mail::button', ['url' => 'localescapemaldives.com/extranet/profile'])
+Complete Your Profile
 @endcomponent
 
 Thanks,<br>
